29abd620b7e6643c13edec08533acd209c4d8151
chaemin1201 <chaemin1201@naver.com>
2026-06-09 01:09:19 +0900

    Fix: import DateTime in database.py

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
